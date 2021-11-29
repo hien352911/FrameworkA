89e7a69cba7c778804d0bb22c9a6fc5932bf9c7a
HienBD <hienbd@techcombank.com.vn>
2021-11-29 15:28:50 +0700

    update

---
+++ FrameworkA/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  FrameworkA
+//
+//  Created by Hien IT. Bui Duy on 29/11/2021.
+//
+
+import Foundation
+
+/// Notice that the struct is public, and the members are implicitly public too.
+public struct Constants {
+    static let shared = Constants()
+    
+    init() {}
+    
+    func foo() {
+        print("Foo")
+    }
+}
