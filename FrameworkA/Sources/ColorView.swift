d1819c158988fac76b0954be9ef4891bd81b6f45
HienBD <hienbd@techcombank.com.vn>
2021-11-29 17:34:30 +0700

    add ColorView

---
+++ FrameworkA/Sources/ColorView.swift
@@ -0,0 +1,29 @@
+//
+//  ColorView.swift
+//  FrameworkA
+//
+//  Created by Hien IT. Bui Duy on 29/11/2021.
+//
+
+import Foundation
+
+class ColorView: UIView {
+    @IBOutlet private weak var contentView: UIView!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        Bundle.main.loadNibNamed(self.className, owner: self)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+}
