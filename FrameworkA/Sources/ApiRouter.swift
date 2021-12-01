05bebe48d03650b44a96db4601e0c17bdf953795
HienBD <hienbd@techcombank.com.vn>
2021-12-01 10:42:43 +0700

    update

---
+++ FrameworkA/Sources/ApiRouter.swift
@@ -0,0 +1,15 @@
+//
+//  ApiRouter.swift
+//  FrameworkA
+//
+//  Created by Hien IT. Bui Duy on 01/12/2021.
+//
+
+import Foundation
+import Alamofire
+
+enum ApiRouter {
+    func request() {
+        _ = AF.request("")
+    }
+}
